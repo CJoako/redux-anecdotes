58b2ece4796905cb4fea1d957ea19742fd181c69
Joaquin Espinoza Rivera <96857078+CJoako@users.noreply.github.com>
2024-04-04 16:45:43 +0000

    EJERCICIO 6.14

--- src/store.jsx
+++ src/store.jsx
@@ -1,12 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import anecdoteReducer from "./reducers/anecdoteReducer";
+import anecdoteReducer, { setAnecdotes } from "./reducers/anecdoteReducer"; // Importa setAnecdotes
 import notificationReducer from "./reducers/notificationReducer";
+import anecdoteService from "./services/anecdotes";
 
 const store = configureStore({
   reducer: {
     anecdotes: anecdoteReducer,
     notification: notificationReducer,
   },
+});
+
+anecdoteService.getAll().then((anecdotes) => {
+  store.dispatch(setAnecdotes(anecdotes)); // Envía la lista completa de anécdotas
 });
 
 export default store;
